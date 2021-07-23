a8aaad6d3ff1abbfa146a8caf028a5d3c33e037f
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-23 18:26:31 +0200

    2 lesson

---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+// __dirname  -- path to current directory
+// __filename
+// require 
+// module
+// process
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process);
+
+setInterval(() => {
+    console.log('b3rking hh');
+}, 1000)
