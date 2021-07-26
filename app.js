7dd8c1dc315bd8c44460722d4372e4c62c9c07f7
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-26 13:36:49 +0200

    why async code on the server

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-
-
-const server = http.createServer((req, res) => {
-    res.end('my own server without any cheat')
-})
-
-server.listen(3200, ()=>{console.log('http://127.1.1:3200')})
